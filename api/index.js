6bce264242cb59c702ecd920cfeb9235a64117df
Aniket Kharad <92918607+aniketkharad@users.noreply.github.com>
2023-09-12 17:28:07 +0530

    Update index.js
    changing api endpoint ordering

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Start at /api end points for RANDOM ACCESS CHANNEL PROCEDURE ! ! !' });
+  });
+
 app.get('/api/', (req, res) => {
   res.json({ message: 'RACH procedure implemtation' });
 });
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Start at /api end points for RANDOM ACCESS CHANNEL PROCEDURE ! ! !' });
-  });
 
 app.listen(port, () => {
 console.log(`Server is running on http://localhost:${port}`);
